df19c13107f89395667128d13bebce0c4abd5d58
Quy <quy.h.le@gmail.com>
2019-03-22 23:50:47 -0400

    integrated intrinio api call

---
+++ usf-asgmt3-api/Integration/intrinioRepo.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.Serialization.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using usf_asgmt3_api.model;
+
+namespace usf_asgmt3_api.Integration
+{
+    public class IntrinioRepo
+    {
+        private string apiUrlV2 = @"https://api-v2.intrinio.com";
+        private string apiKey = "";
+        private static readonly HttpClient client = new HttpClient();
+
+        public async Task<List<Company>> GetCompaniesAsync()
+        {
+            int pageNumber = 1;
+            int pageSize = 100;
+
+            string nextPage = string.Empty;
+
+            var retVal = new List<Company>();
+
+            string endpoint = $"{apiUrlV2}/companies?api_key={apiKey}&page_size={pageSize}";
+            client.DefaultRequestHeaders.Accept.Clear();
+            var serializer = new DataContractJsonSerializer(typeof(CompanyResponse));
+
+            do
+            {
+                try
+                {
+                    if (pageNumber % 100 == 1) // throttle limits: Users enjoying free data feed subscriptions only are limited to 100 requests per second.
+                        Thread.Sleep(1000);
+
+                    var streamTask = client.GetStreamAsync($"{endpoint}&next_page={nextPage}");
+                    var resp = serializer.ReadObject(await streamTask) as CompanyResponse;
+                    
+                    nextPage = resp.next_page;
+
+                    var data = resp.companies.Where(a => !string.IsNullOrEmpty(a.ticker));
+
+                    foreach (var p in data)
+                    {
+                        retVal.Add(p);
+                    }
+                    pageNumber++;
+
+                    if (pageNumber > 2) break; // break out here for first 200 records -- remove this in production plz
+                }
+                catch (Exception ex)
+                {
+                    break;
+                }
+            }
+            while (nextPage != null);
+            
+
+            return retVal;
+        }
+    }
+}
---
+++ usf-asgmt3-api/model/Company.cs
@@ -0,0 +1,13 @@
+﻿namespace usf_asgmt3_api.model
+{
+    public class Company
+    {
+        public string id { get; set; }
+        public string ticker { get; set; }
+        public string name { get; set; }
+        public string lei { get; set; }
+        public string cik { get; set; }
+        public string industry_category { get; set; }
+        public long? market_cap { get; set; }
+    }
+}
